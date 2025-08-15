f207504d740190e90070ec0969df26ab5b23b6c0
joseph thepa <josephmafedi@gmail.com>
2025-08-15 10:51:18 +0200

    wtc

---
+++ deep.py
@@ -0,0 +1,7 @@
+user = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+user = str(user).strip().lower()
+match user:
+    case "42"|"forty-two"|"forty two":
+         print("Yes")
+    case _:
+        print("No")
